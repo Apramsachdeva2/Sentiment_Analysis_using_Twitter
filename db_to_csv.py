2ac7f16e9cee9c09364d83e44c9dc5d1e3d999b6
APRAM SACHDEVA <40246411+Apramsachdeva2@users.noreply.github.com>
2019-03-01 06:15:38 -0800

    Update db_to_csv.py

--- db_to_csv.py
+++ db_to_csv.py
@@ -5,6 +5,6 @@ Created on Wed Feb 27 06:50:30 2019
 @author: Apram Sachdeva
 """
 import dataset
-db = dataset.connect("sqlite:///tweets.db")
-result = db["tweets"].all()
-dataset.freeze(result, format='csv', filename="tweets.csv")
+db = dataset.connect(settings.connection_URL)
+result = db[settings.table_name].all()
+dataset.freeze(result, format='csv', filename=settings.csv_file)
